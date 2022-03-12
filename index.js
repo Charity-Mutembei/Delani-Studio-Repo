3fad493d461b8c88c573ba139d8449e61035d1a1
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-03-13 00:28:04 +0300

    Interaction intiate

--- index.js
+++ index.js
@@ -1,64 +1,78 @@
-// $(document).ready(function(){
-//     $(".dev").click(function(){
-//         $(".development").toggle();
-//     });
-// });
-// $(document).ready(function(){
-//     $(".product").click(function(){
-//         $(".planning").toggle();
-//     });
-// });
+// BUSINESS LOGIC
+$(document).ready(function(){
+    $(".dev").click(function(){
+        $(".development").toggle();
+    });
+});
+$(document).ready(function(){
+    $(".product").click(function(){
+        $(".planning").toggle();
+    });
+});
 
-// $(document).ready(function(){
-//     $(".design").click(function(){
-//         $(".project").toggle();
-//     });
-// });
-// // portfolio 
+$(document).ready(function(){
+    $(".design").click(function(){
+        $(".project").toggle();
+    });
+});
+// portfolio 
 
-// $(document).ready(function(){
-//     $(".firstImage").hover(function(){
-//         $("p.card-text-1").toggle();
-//     });
-// });
-// $(document).ready(function(){
-//     $(".secondImage").hover(function(){
-//         $("p.card-text-2").toggle();
-//     });
-// });
-// $(document).ready(function(){
-//     $(".thirdImage").hover(function(){
-//         $("p.card-text-3").toggle();
-//     });
-// });
-// $(document).ready(function(){
-//     $(".fourthImage").hover(function(){
-//         $("p.card-text-4").toggle();
-//     });
-// });
-// $(document).ready(function(){
-//     $(".fifthImage").hover(function(){
-//         $("p.card-text-5").toggle();
-//     });
-// });
-// $(document).ready(function(){
-//     $(".sixthImage").hover(function(){
-//         $("p.card-text-6").toggle();
-//     });
-// });
-// $(document).ready(function(){
-//     $(".seventhImage").hover(function(){
-//         $("p.card-text-7").toggle();
-//     });
-// });
-// $(document).ready(function(){
-//     $(".eighthImage").hover(function(){
-//         $("p.card-text-8").toggle();
-//     });
-// });
+$(document).ready(function(){
+    $(".firstImage").hover(function(){
+        $("p.card-text-1").toggle();
+    });
+});
+$(document).ready(function(){
+    $(".secondImage").hover(function(){
+        $("p.card-text-2").toggle();
+    });
+});
+$(document).ready(function(){
+    $(".thirdImage").hover(function(){
+        $("p.card-text-3").toggle();
+    });
+});
+$(document).ready(function(){
+    $(".fourthImage").hover(function(){
+        $("p.card-text-4").toggle();
+    });
+});
+$(document).ready(function(){
+    $(".fifthImage").hover(function(){
+        $("p.card-text-5").toggle();
+    });
+});
+$(document).ready(function(){
+    $(".sixthImage").hover(function(){
+        $("p.card-text-6").toggle();
+    });
+});
+$(document).ready(function(){
+    $(".seventhImage").hover(function(){
+        $("p.card-text-7").toggle();
+    });
+});
+$(document).ready(function(){
+    $(".eighthImage").hover(function(){
+        $("p.card-text-8").toggle();
+    });
+});
 
 
+// USER INTERFACE
 
-let user = document.getElementById("inputName").value;
-let email = document.getElementById("inputEmail4").value;
-let comment = document.getElementById("floatingTextarea").value;
+
+$(document).ready(function(){
+    document.getElementById("button").onclick = function(){
+        let user = document.getElementById("inputName").value;
+        let email = document.getElementById("inputEmail4").value;
+        let message = document.getElementById("floatingTextarea").value;
+        
+        if (user != "" && email != "" && message != "") {
+            alert (`Hello ${user} we have received your message. Thank you for reaching out to us.`);
+        } else {alert (`Hello, ${user} check the input boxes and submit again.`);
+    }
+
+
+    }
+});
