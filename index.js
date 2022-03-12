57c2217a9f35bcbc037e957f6027cc061bc44e6f
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-03-12 19:33:20 +0300

    Hover effect section

--- index.js
+++ index.js
@@ -14,3 +14,45 @@ $(document).ready(function(){
         $(".project").toggle();
     });
 });
+// portfolio 
+
+$(document).ready(function(){
+    $(".firstImage").hover(function(){
+        $("p.card-text-1").toggle();
+    });
+});
+$(document).ready(function(){
+    $(".secondImage").hover(function(){
+        $("p.card-text-2").toggle();
+    });
+});
+$(document).ready(function(){
+    $(".thirdImage").hover(function(){
+        $("p.card-text-3").toggle();
+    });
+});
+$(document).ready(function(){
+    $(".fourthImage").hover(function(){
+        $("p.card-text-4").toggle();
+    });
+});
+$(document).ready(function(){
+    $(".fifthImage").hover(function(){
+        $("p.card-text-5").toggle();
+    });
+});
+$(document).ready(function(){
+    $(".sixthImage").hover(function(){
+        $("p.card-text-6").toggle();
+    });
+});
+$(document).ready(function(){
+    $(".seventhImage").hover(function(){
+        $("p.card-text-7").toggle();
+    });
+});
+$(document).ready(function(){
+    $(".eighthImage").hover(function(){
+        $("p.card-text-8").toggle();
+    });
+});
